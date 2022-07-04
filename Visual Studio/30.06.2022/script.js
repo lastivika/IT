8365e21fe1174f811a7df52c174b20f008b1c2b8
lastivika <102419987+lastivika@users.noreply.github.com>
2022-07-04 15:25:16 +0300

    Add files via upload

---
+++ Visual Studio/30.06.2022/script.js
@@ -0,0 +1,118 @@
+// "use strict"
+
+// let a = prompt("Вкажіть ім'я");
+
+// if(a == null){
+//     console.log("Ім'я не може бути пустим")
+// }
+
+// else if (a == "Yulia") {
+//     console.log("Ти є в нашому списку")
+// }
+
+// else if (a ==  "Oleg") {
+//     console.log("Ти є в нашому списку")
+// }
+
+// else if (a == "") {
+//     console.log("Ім'я не може бути пустим")
+// }
+
+// else {
+//     console.log("Вибач, але тебе немає в списку")
+// }
+
+
+// while (true) {
+//     alert('Hello')
+// }
+
+// while(5 > 1){
+//     alert('Hello')
+// }
+
+// while(1){
+//     alert('Hello')
+// }
+
+// let i = 0;
+
+// while (i < 3) {
+//     console.log("Цикл 1 " + i)
+//     //i = i + 1
+//     i++
+
+
+//     let j = 0;
+
+//     while (j < 3){
+//         console.log("Hello " + j)
+//         //j = j + 1
+//         j++
+
+//     }
+// }
+
+// for(let i = 0; i < 13; i++){
+//     num = num + 13
+//     console.log(num)
+// }
+
+// let i = 10
+// for(; i >= 0; ){
+//     console.log(i)
+//     i--
+// }
+
+// for(let i = 10; i >= 0; i--){
+//     console.log(i)
+// }
+
+// let i = 0;
+// do {
+//     console.log(i)
+//     i++
+// }while(i < 3)
+
+// for (let i = 0; i < 15; i++) {
+//     if (i > 3) {
+//         break
+//     }
+//     console.log(i)
+// }
+
+// let num = 5;
+// for (let i = 0; i < 15; i++) {
+//     if (i > num) {
+//         break
+//     }
+//     console.log(i)
+// }
+
+// for(let i = 0; i < 20; i++){
+
+//     if(i == 5|| i == 10|| i == 15){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+
+// while(true){
+//     let a = prompt("Введіть стоп-слово");
+//     if (a == "stop"){
+//         alert("Congrats")
+//         break
+//     }
+// }
+
+// for (let i = 2; i <= 20; i+=2){
+//     console.log(i)
+// }
+
+// for (let i = 2; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//       console.log( i );
+//     }
+//   }
+    
